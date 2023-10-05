149356add34e4e24bffaa8e00b214556ce49c13e
Neeraj Sharma <ns66672@gmail.com>
2023-10-05 12:49:22 +0530

    website_layout

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".wrap-4 .mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+      pagination: {
+        el: ".wrap-4 .swiper-pagination",
+      },
+    },
+  });
